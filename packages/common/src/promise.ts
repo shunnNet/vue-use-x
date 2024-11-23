46f6607ffad1e1efa9f6b08e1c71b6ccc0010b01
shunnNet <wendell20904102@gmail.com>
2024-11-24 00:30:08 +0800

    chore: add type annotation

--- packages/common/src/promise.ts
+++ packages/common/src/promise.ts
@@ -1,8 +1,33 @@
+/**
+ * A deferred promise
+ *
+ * @template T The type of the promise
+ * @property promise The promise
+ * @property _resolve The resolve function
+ * @property _reject The reject function
+ *
+ * @example
+ * const _defer = defer<number>()
+ * _defer.promise.then(value => console.log(value))
+ * _defer._resolve(42)
+ */
 export type TDefer<T = any> = {
   promise: Promise<T>
   _resolve: (value: T) => void
   _reject: (reason?: any) => void
 }
+/**
+ * Creates a deferred promise
+ *
+ * @returns  A deferred promise
+ * @template T The type of the promise
+ * @example
+ *
+ * const _defer = defer<number>()
+ * _defer.promise.then(value => console.log(value))
+ * _defer._resolve(42)
+ *
+ */
 export const defer = <T = any>() => {
   const _defer = {
     resolve: null,
